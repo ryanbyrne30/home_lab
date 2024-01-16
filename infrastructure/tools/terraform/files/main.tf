e34b6254dc16520308f3543834f08fe388849d91
ryanbyrne30 <ryanjbyrne30@gmail.com>
2024-01-15 20:30:27 -0700

    docs(SonarQube): SonarQube IAC

--- infrastructure/tools/terraform/files/main.tf
+++ infrastructure/tools/terraform/files/main.tf
@@ -10,3 +10,64 @@ terraform {
         region = "us-west-1" 
     }
 }
+
+module "k8s_server" {
+  source = "./modules/cloned_vm"
+
+  proxmox_endpoint = var.proxmox_endpoint
+  proxmox_username = var.proxmox_username
+  proxmox_password = var.proxmox_password
+  use_qemu_agent = true 
+
+  node_name = "homelab"
+  name = "k8s-server"
+  description = "Hosts Rancher dashboard to control public and internal clusters"
+  vm_id = 1001
+  template_id = 100
+
+  cpus = 2
+  memory = 4096
+  disk_size = 100
+
+  network_devices = [{
+    address = "192.168.1.5/24"
+    gateway = "192.168.1.1"
+    vlan_id = 1
+  }]
+}
+
+module "k8s_node1" {
+  source = "./modules/cloned_vm"
+
+  proxmox_endpoint = var.proxmox_endpoint
+  proxmox_username = var.proxmox_username
+  proxmox_password = var.proxmox_password
+  use_qemu_agent = true 
+
+  node_name = "homelab"
+  name = "k8s-node1"
+  vm_id = 5003
+  template_id = 100
+
+  cpus = 2
+  memory = 4096
+  disk_size = 100
+
+  network_devices = [{
+    address = "192.168.1.4/24"
+    gateway = "192.168.1.1"
+    vlan_id = 1
+  }]
+}
+
+resource "local_file" "k8s_ansible_inventory" {
+  filename = "output/k8s/inventory.ini"
+  content = <<EOF
+[k8s]
+${join("\n", module.k8s_server.ip)} hostname=k8s-server disk_size=100
+
+[nodes]
+${join("\n", module.k8s_node1.ip)} hostname=k8s-node1 disk_size=100
+
+EOF
+}
